a15816588e019739b6f231284c955a7df86cb1d4
crispenna-123 <cristiano.rosa2010@gmail.com>
2024-10-22 15:23:50 -0300

    Atualizando exp para xp

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ const rl = readline.createInterface({
 function categoriaXP(xp) {
   if (xp < 1000) {
       return "Ferro";
-  } else if (xp >= 1001 && exp <= 2000) {
+  } else if (xp >= 1001 && xp <= 2000) {
       return "Bronze";
-  } else if (xp >= 2001 && exp <= 5000) {
+  } else if (xp >= 2001 && xp <= 5000) {
       return "Prata";
-  } else if (xp >= 5001 && exp <= 7000) {
+  } else if (xp >= 5001 && xp <= 7000) {
       return "Ouro";
-  } else if (xp >= 7001 && exp <= 8000) {
+  } else if (xp >= 7001 && xp <= 8000) {
       return "Platina";
-  } else if (xp >= 8001 && exp <= 9000) {
+  } else if (xp >= 8001 && xp <= 9000) {
       return "Ascendente";
-  } else if (xp >= 9001 && exp <= 10000) {
+  } else if (xp >= 9001 && xp <= 10000) {
       return "Imortal";
   } else {
       return "Radiante";
