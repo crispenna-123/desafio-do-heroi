cc1ec703dd497cfe2ae6ccdb7c845b6f226572cf
Cristiano  Penna <cristiano.rosa2010@gmail.com>
2024-10-23 15:22:11 -0300

    Update index.js
    Adicionando experiência inválida

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ const rl = readline.createInterface({
 
 // Função que verifica o nível do herói com base na experiência
 function categoriaXP(xp) {
-  if (xp < 1000) {
+  if (xp < 0) {
+      return "Experiência inválida";
+  } else if (xp <= 1000) {
       return "Ferro";
-  } else if (xp >= 1001 && xp <= 2000) {
+  } else if (xp <= 2000) {
       return "Bronze";
-  } else if (xp >= 2001 && xp <= 5000) {
+  } else if (xp <= 5000) {
       return "Prata";
-  } else if (xp >= 5001 && xp <= 7000) {
+  } else if (xp <= 7000) {
       return "Ouro";
-  } else if (xp >= 7001 && xp <= 8000) {
+  } else if (xp <= 8000) {
       return "Platina";
-  } else if (xp >= 8001 && xp <= 9000) {
+  } else if (xp <= 9000) {
       return "Ascendente";
-  } else if (xp >= 9001 && xp <= 10000) {
+  } else if (xp <= 10000) {
       return "Imortal";
   } else {
       return "Radiante";
